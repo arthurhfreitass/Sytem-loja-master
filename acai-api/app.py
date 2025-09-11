90bbf2f596c75a6563d28eb3fa040e9d60b6f458
arthurhfreitass <106278534+arthurhfreitass@users.noreply.github.com>
2025-09-11 07:58:17 -0300

    render

--- acai-api/app.py
+++ acai-api/app.py
@@ -13,6 +13,12 @@ sdk = SDK(os.environ.get("ACCESS_TOKEN"))
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
+@app.after_request
+def apply_cors(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
 
 # Criar pagamento PIX
 @app.route("/create_pix_payment", methods=["POST"])
